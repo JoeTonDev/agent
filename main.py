8866565f376e6ebc3d993a865b612e13678cbce4
Joe Ton <joeton07@gmail.com>
2025-01-07 10:33:45 -0800

    Add new logic

--- main.py
+++ main.py
@@ -7,5 +7,21 @@ from langchain_core.messages import BaseMessage
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.checkpoint.memory import MemorySaver
 
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+    
+graph_builder = StateGraph(State)
+    
+llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
 
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+  
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
+graph_builder.add_node("chatbot", chatbot)
 
