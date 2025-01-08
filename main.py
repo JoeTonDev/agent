6c9def65b58e821651766c25d77f9ab48f31246d
Joe Ton <joeton07@gmail.com>
2025-01-08 08:16:32 -0800

    Add new logic

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from typing import Annotated
+import json
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END, add_messages
 from langchain_anthropic import ChatAnthropic
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, ToolMessage
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.checkpoint.memory import MemorySaver
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+
+memory = MemorySaver()
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
@@ -14,14 +19,42 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
     
 graph_builder = StateGraph(State)
-    
+
+tool = TavilySearchResults(max_results=2)
+tools = [tool] 
+tool.invoke("What's a 'node' in LangGraph?")
+
 llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+# Modification: tell the LLM which tools it can call
+llm_with_tools = llm.bind_tools(tools)
 
 def chatbot(state: State):
     return {"messages": [llm.invoke(state["messages"])]}
   
+graph_builder.add_node("chatbot", chatbot)
+  
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+  "chatbot",
+  tools_condition,
+)
+  
 # The first argument is the unique node name
 # The second argument is the function or object that will be called whenever
 # the node is used.
-graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile(
+  checkpointer=memory,
+  interupt_before=["tools"],)
+
+user_input = "I'm learning LangGraph. Could you do some research on it for me?"
+config = {"configurable": {"thread_id": "1"}}
+# The config is the **second positional argument** to stream() or invoke()!
+events = graph.stream({"messages": [("user", user_input)]}, config)
+for event in events:
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
 
